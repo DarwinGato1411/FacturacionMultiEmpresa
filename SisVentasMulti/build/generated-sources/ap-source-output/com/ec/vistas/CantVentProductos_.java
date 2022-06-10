dfac8eaf1d80884fccaae65b93deb58171bb0a4a
Darwin Morocho <darwinvinicio14@gmail.com>
2022-06-09 23:21:16 -0500

    cambios darwin

--- SisVentasMulti/build/generated-sources/ap-source-output/com/ec/vistas/CantVentProductos_.java
+++ SisVentasMulti/build/generated-sources/ap-source-output/com/ec/vistas/CantVentProductos_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-06-09T16:43:57")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-06-09T23:21:07")
 @StaticMetamodel(CantVentProductos.class)
 public class CantVentProductos_ { 
 
