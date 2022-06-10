bb5f856dac4d741857bc25592633cb0a8774234f
Darwin Morocho <darwinvinicio14@gmail.com>
2022-06-09 23:57:48 -0500

    darwin

--- SisVentasMulti/build/generated-sources/ap-source-output/com/ec/entidad/Pedidos_.java
+++ SisVentasMulti/build/generated-sources/ap-source-output/com/ec/entidad/Pedidos_.java
@@ -6,7 +6,15 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
+<<<<<<< HEAD
 @Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-06-09T23:21:08")
+=======
+<<<<<<< HEAD
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-06-09T12:07:19")
+=======
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-06-09T12:22:10")
+>>>>>>> 354e171f0ac97e0eea30625350f80ab04f1e746b
+>>>>>>> b12b595301135eab9facf21ebda3bca3c36999a3
 @StaticMetamodel(Pedidos.class)
 public class Pedidos_ { 
 
